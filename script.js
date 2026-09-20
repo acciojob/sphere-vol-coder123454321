3d558903129a819d537026c0051d44d716bbcbf8
coder123454321 <115930973+coder123454321@users.noreply.github.com>
2026-09-21 00:31:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function volume_sphere() {
+const radius=document.getElementById("radius");
+const valume=document.getElementById("volume");
+const submit=document.getElementById("submit");
+function volume_sphere(){
     //Write your code here
+	event.preventDefault();
+	let r=Number(radius.value);
+	if (r < 0) {
+    alert("Radius cannot be negative");
+    return;
+}
+	let valume_spare=(4/3)*Math.PI*r*r*r;
+	valume.value=valume_spare;
   
 } 
 
